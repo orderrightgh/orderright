bc82bde5f288cff607bfde22f39125c18f736436
Hubert <ansahhubert8588@gmail.com>
2025-04-06 00:53:26 +0000

    Deploying to Render

--- orderright/settings.py
+++ orderright/settings.py
@@ -143,7 +143,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 WHITENOISE_MAX_AGE = 31536000  # 1 year cache
-WHITENOISE_IMMUTABLE_FILE_TEST = lambda path: path.split('/')[-1].split('.')[0] in ('version', 'hash')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, "static/assets/media/") 
 
